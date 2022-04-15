526605059f5c061b2a02316c8850212250d88e17
yoni <yoni@kali>
2022-04-15 05:20:51 +0300

    First Commit

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <div>
+            <h1>thefutur</h1>
+            <nav>
+                <p>courese & Tools</p>
+                <p>Content</p>
+                <p>Our Mission</p>
+            </nav>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header';
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
